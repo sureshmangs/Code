57227fd18de2c8a9730a9bcb22fd37f76bccc053
sureshmangs <sureshmangs1@gmail.com>
2020-01-21 23:09:42 +0530

    practice 21-Jan-2020

---
+++ competitive programming/codeforces/1223A - CME.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin>>n;
+    int a=1,b=1,c;
+
+    while(n){
+        c=a+b;
+        if(c)
+    }
+
+
+return 0;
+}
