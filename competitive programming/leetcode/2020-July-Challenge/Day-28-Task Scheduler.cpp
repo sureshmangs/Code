e1d2c9beba1c5e73fb874621c66ef00784e3bf24
sureshmangs <sureshmangs1@gmail.com>
2020-07-29 21:53:10 +0530

    :rocket: 29-Jul-2020

---
+++ competitive programming/leetcode/2020-July-Challenge/Day-28-Task Scheduler.cpp
@@ -0,0 +1,64 @@
+You are given a char array representing tasks CPU need to do. It contains capital letters A to Z where each letter represents a different task. Tasks could be done without the original order of the array. Each task is done in one unit of time. For each unit of time, the CPU could complete either one task or just be idle.
+
+However, there is a non-negative integer n that represents the cooldown period between two same tasks (the same letter in the array), that is that there must be at least n units of time between any two same tasks.
+
+You need to return the least number of units of times that the CPU will take to finish all the given tasks.
+
+
+
+Example 1:
+
+Input: tasks = ["A","A","A","B","B","B"], n = 2
+Output: 8
+Explanation:
+A -> B -> idle -> A -> B -> idle -> A -> B
+There is at least 2 units of time between any two same tasks.
+Example 2:
+
+Input: tasks = ["A","A","A","B","B","B"], n = 0
+Output: 6
+Explanation: On this case any permutation of size 6 would work since n = 0.
+["A","A","A","B","B","B"]
+["A","B","A","B","A","B"]
+["B","B","B","A","A","A"]
+...
+And so on.
+Example 3:
+
+Input: tasks = ["A","A","A","A","A","A","B","C","D","E","F","G"], n = 2
+Output: 16
+Explanation:
+One possible solution is
+A -> B -> C -> A -> D -> E -> A -> F -> G -> A -> idle -> idle -> A -> idle -> idle -> A
+
+
+Constraints:
+
+The number of tasks is in the range [1, 10000].
+The integer n is in the range [0, 100].
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        if(n==0) return (int)tasks.size();
+        map<char,int> freq;
+        int max_freq = 0,inc=0;
+        for(int x : tasks) {freq[x]++; max_freq = max(max_freq, freq[x]);}
+        int idle_time = (max_freq-1)*(n+1)+1;
+        for(auto it: freq){
+            if(it.second==max_freq) inc++;
+        }
+        int ans = idle_time+(inc-1);
+        return max(ans, (int)tasks.size());
+    }
+};
