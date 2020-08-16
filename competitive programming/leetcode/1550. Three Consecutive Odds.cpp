8d26ede99855a24ae0fd5d047276801697a209b0
sureshmangs <sureshmangs1@gmail.com>
2020-08-16 22:28:24 +0530

    :rocket: 16-Aug-2020

---
+++ competitive programming/leetcode/1550. Three Consecutive Odds.cpp
@@ -0,0 +1,39 @@
+Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
+
+
+Example 1:
+
+Input: arr = [2,6,4,1]
+Output: false
+Explanation: There are no three consecutive odds.
+Example 2:
+
+Input: arr = [1,2,34,3,4,5,7,23,12]
+Output: true
+Explanation: [5,7,23] are three consecutive odds.
+
+
+Constraints:
+
+1 <= arr.length <= 1000
+1 <= arr[i] <= 1000
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int n=arr.size();
+        if(n<=2) return false;
+        for(int i=0;i<n-2;i++){
+            if(arr[i]%2!=0 && arr[i+1]%2!=0 && arr[i+2]%2!=0)
+                return true;
+        }
+        return false;
+    }
+};
---
+++ competitive programming/leetcode/435. Non-overlapping Intervals.cpp
@@ -0,0 +1,71 @@
+Given a collection of intervals, find the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.
+
+
+
+Example 1:
+
+Input: [[1,2],[2,3],[3,4],[1,3]]
+Output: 1
+Explanation: [1,3] can be removed and the rest of intervals are non-overlapping.
+Example 2:
+
+Input: [[1,2],[1,2],[1,2]]
+Output: 2
+Explanation: You need to remove two [1,2] to make the rest of intervals non-overlapping.
+Example 3:
+
+Input: [[1,2],[2,3]]
+Output: 0
+Explanation: You don't need to remove any of the intervals since they're already non-overlapping.
+
+
+Note:
+
+You may assume the interval's end point is always bigger than its start point.
+Intervals like [1,2] and [2,3] have borders "touching" but they don't overlap each other.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+
+    static bool comp(vector<int> a, vector<int> b){
+        return a[1] < b[1];
+    }
+
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        int res=0;
+        int n=intervals.size();
+        if(n==0) return 0;
+
+        sort(intervals.begin(), intervals.end(), comp);
+
+
+        pair<int, int> cur;
+        cur.first=intervals[0][0];
+        cur.second=intervals[0][1];
+
+        for(int i=1;i<n;i++){
+            if(cur.second>intervals[i][0]){
+                res++;
+            } else {
+                cur.first=intervals[i][0];
+                cur.second=intervals[i][1];
+            }
+        }
+        return res;
+    }
+};
