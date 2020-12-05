bf18add177b496ae4aff9498740c025c14a2f967
sureshmangs <sureshmangs1@gmail.com>
2020-12-05 15:44:07 +0530

    :rocket: 05-Dec-2020

---
+++ competitive programming/leetcode/382. Linked List Random Node.cpp
@@ -0,0 +1,57 @@
+Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
+
+Follow up:
+What if the linked list is extremely large and its length is unknown to you? Could you solve this efficiently without using extra space?
+
+Example:
+
+// Init a singly linked list [1,2,3].
+ListNode head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+Solution solution = new Solution(head);
+
+// getRandom() should return either 1, 2, or 3 randomly. Each element should have equal probability of returning.
+solution.getRandom();
+
+
+
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    vector<int> v;
+
+    Solution(ListNode* head) {
+        while(head){
+            v.push_back(head->val);
+            head=head->next;
+        }
+    }
+
+    /** Returns a random node's value. */
+    int getRandom() {
+        int n=v.size();
+        return v[rand()%n];
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(head);
+ * int param_1 = obj->getRandom();
+ */
---
+++ competitive programming/leetcode/897. Increasing Order Search Tree.cpp
@@ -0,0 +1,62 @@
+Given the root of a binary search tree, rearrange the tree in in-order so that the leftmost node in the tree is now the root of the tree, and every node has no left child and only one right child.
+
+
+
+Example 1:
+
+
+Input: root = [5,3,6,2,4,null,8,1,null,null,null,7,9]
+Output: [1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
+Example 2:
+
+
+Input: root = [5,1,7]
+Output: [1,null,5,null,7]
+
+
+Constraints:
+
+The number of nodes in the given tree will be in the range [1, 100].
+0 <= Node.val <= 1000
+
+
+
+
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    void inorder(TreeNode *root, vector<int> &v){
+        if(!root) return;
+        inorder(root->left, v);
+        v.push_back(root->val);
+        inorder(root->right, v);
+    }
+
+    TreeNode* increasingBST(TreeNode* root) {
+        vector<int> v;
+        inorder(root, v);
+        TreeNode *res= new TreeNode(0); //dummy
+        TreeNode *curr=res;
+        for(auto &x: v){
+            curr->right=new TreeNode(x);
+            curr=curr->right;
+        }
+
+        return res->right;
+    }
+};
